50791928a118d383885a80fbd0215b7610fe7b66
Jordan <penttijordo@gmail.com>
2023-08-08 14:07:38 -0400

    begin spots router construction

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const { Spot } = require('../../db/models');
+
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+
+
+
+module.exports = router;
